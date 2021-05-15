c9584578c4168ed201bc2f6dd81d074bbf21f2d2
seyed <mohammadjavadsabagh@gmail.com>
2021-05-15 11:46:59 +0430

    asdasd

--- app/src/main/java/com/google/hotelabbasi/MainActivity.java
+++ app/src/main/java/com/google/hotelabbasi/MainActivity.java
@@ -69,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
         shimmerFrameLayout.startShimmer();
 
+
         ImageSliderTop();
         jazebeViewPager();
         EghamatViewPager();
