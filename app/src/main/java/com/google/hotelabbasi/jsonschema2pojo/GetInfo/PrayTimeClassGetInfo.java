4ec810aeb57ce69fcb1d7ebb923fa81ea6d89e48
seyed <mohammadjavadsabagh@gmail.com>
2021-06-13 12:32:09 +0430

    add Gson and Set GetInfo

---
+++ app/src/main/java/com/google/hotelabbasi/jsonschema2pojo/GetInfo/PrayTimeClassGetInfo.java
@@ -0,0 +1,384 @@
+package com.google.hotelabbasi.jsonschema2pojo;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class PrayTimeClassGetInfo {
+
+    @SerializedName("kind_id")
+    @Expose
+    private Integer kindId;
+    @SerializedName("C")
+    @Expose
+    private Double c;
+    @SerializedName("D")
+    @Expose
+    private Double d;
+    @SerializedName("placeMode")
+    @Expose
+    private String placeMode;
+    @SerializedName("city")
+    @Expose
+    private String city;
+    @SerializedName("onlineReservation")
+    @Expose
+    private Integer onlineReservation;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("created_at")
+    @Expose
+    private String createdAt;
+    @SerializedName("cityId")
+    @Expose
+    private Integer cityId;
+    @SerializedName("video")
+    @Expose
+    private String video;
+    @SerializedName("vabastegi")
+    @Expose
+    private String vabastegi;
+    @SerializedName("seoTitle")
+    @Expose
+    private String seoTitle;
+    @SerializedName("error")
+    @Expose
+    private Boolean error;
+    @SerializedName("momtaz")
+    @Expose
+    private Boolean momtaz;
+    @SerializedName("seen")
+    @Expose
+    private Integer seen;
+    @SerializedName("room_num")
+    @Expose
+    private Integer roomNum;
+    @SerializedName("file")
+    @Expose
+    private String file;
+    @SerializedName("updated_at")
+    @Expose
+    private String updatedAt;
+    @SerializedName("rate")
+    @Expose
+    private Double rate;
+    @SerializedName("reviewCount")
+    @Expose
+    private Integer reviewCount;
+    @SerializedName("fullRate")
+    @Expose
+    private Integer fullRate;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("state")
+    @Expose
+    private String state;
+    @SerializedName("keyword")
+    @Expose
+    private String keyword;
+    @SerializedName("slug")
+    @Expose
+    private String slug;
+    @SerializedName("address")
+    @Expose
+    private String address;
+    @SerializedName("reserveId")
+    @Expose
+    private Integer reserveId;
+    @SerializedName("author")
+    @Expose
+    private Integer author;
+    @SerializedName("stateId")
+    @Expose
+    private Integer stateId;
+    @SerializedName("site")
+    @Expose
+    private String site;
+    @SerializedName("rate_count")
+    @Expose
+    private Integer rateCount;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+    @SerializedName("meta")
+    @Expose
+    private String meta;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    public Integer getKindId() {
+        return kindId;
+    }
+
+    public void setKindId(Integer kindId) {
+        this.kindId = kindId;
+    }
+
+    public Double getC() {
+        return c;
+    }
+
+    public void setC(Double c) {
+        this.c = c;
+    }
+
+    public Double getD() {
+        return d;
+    }
+
+    public void setD(Double d) {
+        this.d = d;
+    }
+
+    public String getPlaceMode() {
+        return placeMode;
+    }
+
+    public void setPlaceMode(String placeMode) {
+        this.placeMode = placeMode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getOnlineReservation() {
+        return onlineReservation;
+    }
+
+    public void setOnlineReservation(Integer onlineReservation) {
+        this.onlineReservation = onlineReservation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public String getVabastegi() {
+        return vabastegi;
+    }
+
+    public void setVabastegi(String vabastegi) {
+        this.vabastegi = vabastegi;
+    }
+
+    public String getSeoTitle() {
+        return seoTitle;
+    }
+
+    public void setSeoTitle(String seoTitle) {
+        this.seoTitle = seoTitle;
+    }
+
+    public Boolean getError() {
+        return error;
+    }
+
+    public void setError(Boolean error) {
+        this.error = error;
+    }
+
+    public Boolean getMomtaz() {
+        return momtaz;
+    }
+
+    public void setMomtaz(Boolean momtaz) {
+        this.momtaz = momtaz;
+    }
+
+    public Integer getSeen() {
+        return seen;
+    }
+
+    public void setSeen(Integer seen) {
+        this.seen = seen;
+    }
+
+    public Integer getRoomNum() {
+        return roomNum;
+    }
+
+    public void setRoomNum(Integer roomNum) {
+        this.roomNum = roomNum;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Double getRate() {
+        return rate;
+    }
+
+    public void setRate(Double rate) {
+        this.rate = rate;
+    }
+
+    public Integer getReviewCount() {
+        return reviewCount;
+    }
+
+    public void setReviewCount(Integer reviewCount) {
+        this.reviewCount = reviewCount;
+    }
+
+    public Integer getFullRate() {
+        return fullRate;
+    }
+
+    public void setFullRate(Integer fullRate) {
+        this.fullRate = fullRate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getReserveId() {
+        return reserveId;
+    }
+
+    public void setReserveId(Integer reserveId) {
+        this.reserveId = reserveId;
+    }
+
+    public Integer getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Integer author) {
+        this.author = author;
+    }
+
+    public Integer getStateId() {
+        return stateId;
+    }
+
+    public void setStateId(Integer stateId) {
+        this.stateId = stateId;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public Integer getRateCount() {
+        return rateCount;
+    }
+
+    public void setRateCount(Integer rateCount) {
+        this.rateCount = rateCount;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMeta() {
+        return meta;
+    }
+
+    public void setMeta(String meta) {
+        this.meta = meta;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
