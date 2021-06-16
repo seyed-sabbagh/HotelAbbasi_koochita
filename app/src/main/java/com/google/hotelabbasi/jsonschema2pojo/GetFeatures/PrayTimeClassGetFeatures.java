46044655816dd73818986087e9e57253e4f823dd
seyed <mohammadjavadsabagh@gmail.com>
2021-06-16 16:48:20 +0430

    ...

--- app/src/main/java/com/google/hotelabbasi/jsonschema2pojo/GetFeatures/PrayTimeClassGetFeatures.java
+++ app/src/main/java/com/google/hotelabbasi/jsonschema2pojo/GetFeatures/PrayTimeClassGetFeatures.java
@@ -1,9 +1,10 @@
 
 package com.google.hotelabbasi.jsonschema2pojo.GetFeatures;
 
-import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
 
 public class PrayTimeClassGetFeatures {
 
